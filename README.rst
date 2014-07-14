9eb3bb603ac5a5cf8e14b7ceb4a6603a337817cd
David Lord <davidism@gmail.com>
2014-07-13 20:15:58 -0700

    remove old version

---
+++ README.rst
@@ -0,0 +1,12 @@
+**so**\ python
+==============
+
+Cabbage, World!
+
+Welcome to **so**\ python, the website of the Python community on `Stack Overflow <so_>`_.
+
+Join us for chat in the `Python room <chat_>`_!
+
+
+.. _so: https://stackoverflow.com/
+.. _chat: http://chat.stackoverflow.com/rooms/6/python
--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name='sopy',
-    version='0.0.0',
+    version='1.0.0',
     packages=find_packages(),
     url='http://sopython.com/',
     license='BSD',
-    author='Ffisegydd',
-    author_email='ffisegydd@sopython.com',
+    author='David Lord',
+    author_email='davidism@gmail.com',
     description='The productive programming cabbage website.',
-    entry_points={
-        'console_scripts': [
-            'sopy = sopy.manage:cli.main',
-        ],
-    },
+    entry_points={'console_scripts': ['sopy = sopy.manage:cli.main']},
+    install_requires=requirements
 )
