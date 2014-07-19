bc645c83b2e4008b5728a6ad8c8c1bf1c74615ce
Nafiul Islam <gamesbrainiac@gmail.com>
2014-07-20 03:18:56 +0600

    Added my name to the list
    Making sure other names follow PEP8 convention :)

--- allowed.py
+++ allowed.py
@@ -1,14 +1,15 @@
 ALLOWED = {
-	1903116, #thefoutheye
-	3005188, #Stewie
-	2188562, #Peter
-	1252759, #Jon
-	953482,  #Kevin
-	100297,  #Martijn
-	1578604, #Jerry
-	487339, #DSM		
-	918959, #Antti
-	1014938, #Zero
-	400617, #davidism
-	559633, #tristan
+	1903116, # thefoutheye
+	3005188, # Stewie
+	2188562, # Peter
+	1252759, # Jon
+	953482,  # Kevin
+	100297,  # Martijn
+	1578604, # Jerry
+	487339,  # DSM		
+	918959,  # Antti
+	1014938, # Zero
+	400617,  # davidism
+	559633,  # tristan
+	1624921, # gamesbrainiac
 }
