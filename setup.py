cb0de7aa45722616a96924c2a42772747186cc32
David Lord <davidism@gmail.com>
2014-08-25 21:24:46 -0700

    begin 1.2-dev

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='sopy',
-    version='1.1',
+    version='1.2-dev',
     packages=find_packages(),
     include_package_data=True,
     url='http://sopython.com/',
