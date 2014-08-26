fcbbe3ae61b6ef6a0aebd139bd953867b6993dad
David Lord <davidism@gmail.com>
2014-08-25 21:22:58 -0700

    add docs to build, release 1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='sopy',
-    version='1.1-dev',
+    version='1.1',
     packages=find_packages(),
     include_package_data=True,
     url='http://sopython.com/',
