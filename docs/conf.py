1046f2212cb805f931a8931cde6eaeeddacc3bb4
David Lord <davidism@gmail.com>
2014-09-05 19:48:32 -0700

    begin 1.3-dev

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = '2014, sopython organization'
 # built documents.
 #
 # The short X.Y version.
-version = '1.2'
+version = '1.3-dev'
 # The full version, including alpha/beta/rc tags.
 release = version
 
