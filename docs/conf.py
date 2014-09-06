01083cca96d253930bc464920cc9080f86e401ab
David Lord <davidism@gmail.com>
2014-09-05 19:46:42 -0700

    release 1.2

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = '2014, sopython organization'
 # built documents.
 #
 # The short X.Y version.
-version = '1.2-dev'
+version = '1.2'
 # The full version, including alpha/beta/rc tags.
 release = version
 
