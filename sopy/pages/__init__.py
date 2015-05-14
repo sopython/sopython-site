a6cb427e6821f1dc38b67a63141b294b4a5bb44e
David Lord <davidism@gmail.com>
2015-05-14 08:31:16 -0700

    chatroom url alias new icon that doesn't look like two snakes update requirements

--- sopy/pages/__init__.py
+++ sopy/pages/__init__.py
@@ -6,3 +6,6 @@ bp = Blueprint('pages', __name__)
 @bp.record_once
 def register(state):
     from sopy.pages import views
+
+    # alias /pages/chatroom to /chatroom
+    state.app.add_url_rule('/chatroom', 'pages.page', view_func=views.page, defaults={'name': 'chatroom'})
