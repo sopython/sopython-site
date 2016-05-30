6645e5b1e13cd61ef21401a69fb4d07c12398057
David Lord <davidism@gmail.com>
2016-05-29 17:18:47 -0700

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='sopy',
-    version='1.6',
+    version='1.7',
     packages=find_packages(),
     include_package_data=True,
     url='http://sopython.com/',
