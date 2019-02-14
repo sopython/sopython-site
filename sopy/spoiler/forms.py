a552b33bd18f71f98e933c22a72f94b02fa0c6ef
Patrick Westerhoff <PatrickWesterhoff@gmail.com>
2019-02-14 16:17:21 +0100

    Fix spoiler submit
    Remove validation on the text field to prevent the validation that is
    built into browsers from preventing the form submission.
    
    The form submit would usually trigger a function that copies over the
    content from the fancy editor into the plain (but hidden) text field.
    But since the form submission is prevented, that function never runs and
    never makes the form valid even if content was added.
    
    The downside of this change is that you can submit with an empty text
    field but I would say that is not a real issue for users of this
    service.

--- sopy/spoiler/forms.py
+++ sopy/spoiler/forms.py
@@ -1,6 +1,5 @@
 from flask_wtf import FlaskForm
 from wtforms import TextAreaField
-from wtforms.validators import InputRequired
 
 class SpoilerForm(FlaskForm):
-    message = TextAreaField(validators=[InputRequired()])
+    message = TextAreaField(validators=[])
